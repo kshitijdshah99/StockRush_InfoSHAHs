706ddbae694708934be071781bff2bd3cbfc81f1
kshitijdshah99 <144168166+kshitijdshah99@users.noreply.github.com>
2024-01-17 18:42:07 +0530

    Update refreshTokenController.js

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -7,30 +7,16 @@ const handleRefreshToken = async (req, res) => {
     if(!cookies?.jwt) return res.sendStatus(401);
     
     const refreshToken = cookies.jwt;
-    res.clearCookie('jwt', {httpOnly: true, sameSite:'None'});
-    
-    //check for username in DB
-    const foundUser = await User.findOne({refreshToken}).exec();
-    if(!foundUser){ 
-        //if no user but someone is trying to use the refresh token
-        jwt.verify(
-            refreshToken,
-            process.env.REFRESH_TOKEN_SECRET,
-            async (err, decoded) => {
-                if(err) return res.sendStatus(403); //Forbidden
-                //delete refreshToken from DB
-                const result = await User.updateOne(
-                    {username: decoded.userInfo.username},
-                    {$set: {refreshToken: []}}
-                );
-                console.log(result);
-                if(!result) return res.sendStatus(500);
-            }
-        );
-        return res.sendStatus(403);
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None' });
+
+    // Check for username, email, and password in DB
+    const foundUser = await User.findOne({ refreshToken }).exec();
+    if (!foundUser || foundUser.email_id !== req.body.email_id || foundUser.password !== req.body.password) {
+        return res.sendStatus(401); // Unauthorized
     }
+
     try {
-        //compare refresh token
+        // Compare refresh token
         const newRefreshTokenArray = foundUser.refreshToken.filter(token => token !== refreshToken);
 
         jwt.verify(
@@ -38,44 +24,43 @@ const handleRefreshToken = async (req, res) => {
             process.env.REFRESH_TOKEN_SECRET,
             async (err, decoded) => {
                 if(err) {
-                    //delete wrong refreshToken from DB if user found
+                    // Delete wrong refreshToken from DB if user found
                     foundUser.refreshToken = newRefreshTokenArray;
                     const result = await foundUser.save();
                     console.log(result);
-                    if(!result) return res.sendStatus(500);
-                    return res.sendStatus(403); //Forbidden
+                    return res.sendStatus(403); // Forbidden
                 }
 
-                if(err || foundUser.username !== decoded.userInfo.username) return res.sendStatus(403);
+                if (err || foundUser.username !== decoded.userInfo.username) return res.sendStatus(403);
 
                 const roles = Object.values(foundUser.roles);
                 const accessToken = jwt.sign(
                     {
-                        "userInfo":{
+                        "userInfo": {
                             "username": foundUser.username,
                             "roles": roles
                         }
                     },
                     process.env.ACCESS_TOKEN_SECRET,
-                    {expiresIn: '3d'}
+                    { expiresIn: '3d' }
                 );
                 const newRefreshToken = jwt.sign(
                     {
                         "userInfo": {
-                            "username": foundUser.username, 
+                            "username": foundUser.username,
                             "roles": roles
                         }
-                    }, 
-                    process.env.REFRESH_TOKEN_SECRET, 
-                    {expiresIn: '15d'}
+                    },
+                    process.env.REFRESH_TOKEN_SECRET,
+                    { expiresIn: '15d' }
                 );
-                //save refresh token to current user
-                foundUser.refreshToken= [...newRefreshTokenArray, newRefreshToken];
+                // Save refresh token to the current user
+                foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
                 const result = await foundUser.save();
                 console.log(result);
-                res.cookie('jwt', newRefreshToken, {httpOnly: true, sameSite:'None', maxAge: 24*60*60*1000});
-    
-                res.status(200).json({roles,accessToken});
+                res.cookie('jwt', newRefreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+
+                res.status(200).json({ roles, accessToken });
             }
         );
     }
@@ -84,4 +69,4 @@ const handleRefreshToken = async (req, res) => {
     }
 }
 
-module.exports =  handleRefreshToken;
+module.exports = handleRefreshToken;
