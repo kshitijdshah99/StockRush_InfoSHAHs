843a993f6be68f3e8552a9c2509255e82fee9701
kshitijdshah99 <144168166+kshitijdshah99@users.noreply.github.com>
2024-02-08 17:33:15 +0530

    Delete routes directory

--- routes/logout.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const handleLogout = require('../controllers/logoutController');
-
-router.get('/', handleLogout)
-
-module.exports = router;
--- routes/refresh.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const handleRefreshToken = require('../controllers/refreshTokenController');
-
-router.get('/', handleRefreshToken)
-
-module.exports = router;
