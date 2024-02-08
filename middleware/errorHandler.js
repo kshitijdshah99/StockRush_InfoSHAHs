123610d037d3160c39c51a1933a887cf360ed325
kshitijdshah99 <144168166+kshitijdshah99@users.noreply.github.com>
2024-02-08 17:35:34 +0530

    Delete middleware directory

--- middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const {logEvents} = require('./logEvents');
-const errorHandler = (err, req, res, next) => {
-    logEvents(`${err.name}:${err.message}`, 'errorLog.txt')
-    console.log(err.stack);
-    res.status(500).send(err.message);
-}
-module.exports = errorHandler;
