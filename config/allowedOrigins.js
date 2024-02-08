6a48607788f918be936d0f1354e72b3cedd3758a
kshitijdshah99 <144168166+kshitijdshah99@users.noreply.github.com>
2024-02-08 17:32:06 +0530

    Delete config directory

--- config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-// Need to edit this file based on frontend
-const allowedOrigins = [
-    'http://localhost:3500', 
-    'http://localhost:3000', 
-    'http://localhost:4200'
-];
-
-module.exports = allowedOrigins;
