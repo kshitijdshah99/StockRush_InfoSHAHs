a993756d144f8f11f086a3c56e1f12637aeb09dd
kshitijdshah99 <144168166+kshitijdshah99@users.noreply.github.com>
2024-02-08 17:35:46 +0530

    Delete routes directory

--- routes/user_profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { post_user_profile, get_user_profile, patch_user_profile } = require('../controllers/user_profileControllers');
-const router = express.Router();
-
-router.post('/post/up', post_user_profile);
-router.get('/get/up/:profile_id', get_user_profile);
-router.patch('/patch/up/:profile_id', patch_user_profile);
-
-module.exports = router;
