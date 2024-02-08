9a01450c022544435e73daa54b60e8e013211a0f
kshitijdshah99 <144168166+kshitijdshah99@users.noreply.github.com>
2024-02-08 23:58:20 +0530

    Add files via upload

---
+++ middleware/errorHandler.js
@@ -0,0 +1,7 @@
+const {logEvents} = require('./logEvents');
+const errorHandler = (err, req, res, next) => {
+    logEvents(`${err.name}:${err.message}`, 'errorLog.txt')
+    console.log(err.stack);
+    res.status(500).send(err.message);
+}
+module.exports = errorHandler;
