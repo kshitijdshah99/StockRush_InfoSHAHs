77c46059f7e78975a325c983eb5433fc94f9102c
kshitijdshah99 <144168166+kshitijdshah99@users.noreply.github.com>
2024-02-08 18:09:11 +0530

    Add files via upload

---
+++ allowedOrigins.js
@@ -0,0 +1,8 @@
+// Need to edit this file based on frontend
+const allowedOrigins = [
+    'http://localhost:3500', 
+    'http://localhost:3000', 
+    'http://localhost:4200'
+];
+
+module.exports = allowedOrigins;
